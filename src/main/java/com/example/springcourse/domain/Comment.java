3db7f766ca5ab90bd08fd7a9b4097b641628d875
Nikita Goloveshko <ng4381@mail.ru>
2021-12-24 17:20:59 +0300

    Adding mappings

--- src/main/java/com/example/springcourse/domain/Comment.java
+++ src/main/java/com/example/springcourse/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @NoArgsConstructor
@@ -18,4 +19,6 @@ public class Comment {
     private String body;
 
     // link
+    @ManyToOne
+    private Link link;
 }
--- src/main/java/com/example/springcourse/domain/Link.java
+++ src/main/java/com/example/springcourse/domain/Link.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -19,4 +22,6 @@ public class Link {
     private String url;
 
     // comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 }
