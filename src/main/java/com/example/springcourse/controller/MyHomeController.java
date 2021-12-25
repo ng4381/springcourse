a7e8cf299eb363e4c0de7641b5042f91bd1b4219
Nikita Goloveshko <ng4381@mail.ru>
2021-12-25 18:55:12 +0300

    Forget to make a branch fo view))

--- src/main/java/com/example/springcourse/controller/MyHomeController.java
+++ src/main/java/com/example/springcourse/controller/MyHomeController.java
@@ -9,10 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MyHomeController {
 
+    /*
     @GetMapping("/")
     public String home(Model model, HttpServletRequest request) {
         model.addAttribute("message", "Hello World");
         return "index";
     }
+     */
 
 }
