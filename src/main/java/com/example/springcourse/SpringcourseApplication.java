937159a0c1a79d5583a04a710eed41b2a5eb17c1
Nikita Goloveshko <ng4381@mail.ru>
2021-12-30 16:52:38 +0300

    Actuator endponts

--- src/main/java/com/example/springcourse/SpringcourseApplication.java
+++ src/main/java/com/example/springcourse/SpringcourseApplication.java
@@ -14,8 +14,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableConfigurationProperties(SpringitProperties.class)
+//@EnableJpaAuditing
 @SpringBootApplication
 public class SpringcourseApplication {
 
